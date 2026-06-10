1925e8c4a44a256c0b563ae0557dbe53cf2f0cb0
dmitriikushnir <student95tnu@gmail.com>
2026-06-10 10:39:43 +0300

    manifest copying simplified

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "./manifest.json",
-          dest: "",
-        },
-      ],
-    }),
   ],
 });
